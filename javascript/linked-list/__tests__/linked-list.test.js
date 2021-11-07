067e1fe09384f3199400c23e3ce6805beb16bee0
Marwan-Zakia <marwanamir.ma@gmail.com>
2021-11-07 23:27:55 +0200

    new

--- javascript/linked-list/__tests__/linked-list.test.js
+++ javascript/linked-list/__tests__/linked-list.test.js
@@ -53,8 +53,53 @@ it('Will return true when finding a value within the linked list that exists', (
     let ll = new LinkedList();
     ll.insert('a');
     ll.insert('b');
-    ll.insert('c');
-    expect(ll.to_string()).toBe('{a} -> {b} -> {c} -> NULL');
+    ll.insert('c');           
+    expect(ll.to_string()).toBe('head{a} -> {b} -> {c} -> X');
   });
+  
+  it('it adds Multiple elements to the end of the list', () => {
+    const ll = new LinkedList();
+    ll.insert(10)
+    ll.insert(20)
+    ll.insert(30)
+    ll.append(40)
+    expect(ll.head.value).toEqual(10)
+    expect(ll.head.next.value).toEqual(20)
+    expect(ll.head.next.next.value).toEqual(30)
+    expect(ll.head.next.next.next.value).toEqual(40)
+  })
+  
+  it('it adds Multiple elements to the end of the list', () => {
+    const ll = new LinkedList();
+    ll.insert(1)
+    ll.insert(3)
+    ll.insert(2)
+    ll.append(5)
+    
+    expect(ll.to_string()).toEqual('head{1} -> {3} -> {2} -> {5} -> X')
+  })
+  it('it adds a new node before the last node', () => {
+    const ll = new LinkedList();
+    ll.insert(1)
+    ll.insert(2)
+    ll.insert(3)
+    ll.insertBefore(5,2)
+    expect(ll.to_string()).toEqual('head{1} -> {5} -> {2} -> {3} -> X')
+  
+  })
+  it('it adds a new node after the last node', () => {
+    const ll = new LinkedList();
+    ll.insert(1)
+    ll.insert(2)
+    ll.insert(3)
+    ll.insertAfter(5,2)
+    expect(ll.to_string()).toEqual('head{1} -> {5} -> {2} -> {3} -> X')
+  })
+  
+  
+  
+  
+  
+  
 
 });
